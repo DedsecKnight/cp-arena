93346f134afdf23c0fa207db173b98148f533897
Mike Nguyen <mikenguyen7268@gmail.com>
2020-12-21 17:48:08 -0600

    Initial setup for File Submission

--- routes/api/submissions.js
+++ routes/api/submissions.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const upload = require('express-fileupload');
 const router = express.Router();
+
+router.use(upload());
 
 // @route   /api/submissions/
 // @desc    Test route
 // @access  Public
 router.get('/', (req, res) => {
     res.send("Submission route OK");
+})
+
+router.post('/', async (req, res) => {
+    let file = req.files.submission;
+    try {
+        await file.mv('submissions/' + file.name);    
+        res.send("Success");
+    } 
+    catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+    }
 })
 
 module.exports = router;
