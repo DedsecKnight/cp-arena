ba5de577e74db5385493dd06ebc89b1a399ec58a
Mike Nguyen <mikenguyen7268@gmail.com>
2021-01-05 13:06:57 -0600

    Created Problem.js

---
+++ client/src/components/layout/Problem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Problem = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Problem.propTypes = {
+
+}
+
+export default Problem
