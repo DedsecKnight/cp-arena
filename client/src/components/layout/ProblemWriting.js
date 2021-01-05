58480815acd39ab2ee016cd7ebbd62bccbeaeaf6
Mike Nguyen <mikenguyen7268@gmail.com>
2021-01-05 14:41:29 -0600

    Fixed Problem Writing HTML to be compatible with JSX

--- client/src/components/layout/ProblemWriting.js
+++ client/src/components/layout/ProblemWriting.js
@@ -4,63 +4,63 @@ import React, { Fragment } from 'react'
 const ProblemWriting = () => {
     return (
         <Fragment>
-            <h2 class="my-5">Propose a problem here</h2>
-            <form class="cparena-form">
-                <div class="form-group">
-                    <input type="text" class="form-control" name="name" placeholder="Problem name" />
+            <h2 className="my-5">Propose a problem here</h2>
+            <form className="cparena-form">
+                <div className="form-group">
+                    <input type="text" className="form-control" name="name" placeholder="Problem name" />
                 </div>
-                <div class="form-group">
-                    <select name="difficulty" class="form-control">
+                <div className="form-group">
+                    <select name="difficulty" className="form-control">
                         <option selected>Choose a difficulty</option>
                         <option value="easy">Easy</option>
                         <option value="normal">Normal</option>
                         <option value="hard">Hard</option>
                     </select>
                 </div>
-                <div class="form-group">
-                    <textarea name="statement" cols="30" rows="10" class="form-control" placeholder="Problem Statement"></textarea>
+                <div className="form-group">
+                    <textarea name="statement" cols="30" rows="10" className="form-control" placeholder="Problem Statement"></textarea>
                 </div>
-                <div class="form-group">
-                    <textarea name="inputSpec" cols="30" rows="5" class="form-control" placeholder="Input description"></textarea>
+                <div className="form-group">
+                    <textarea name="inputSpec" cols="30" rows="5" className="form-control" placeholder="Input description"></textarea>
                 </div>
-                <div class="form-group">
-                    <textarea name="outputSpec" cols="30" rows="5" class="form-control" placeholder="Output description"></textarea>
+                <div className="form-group">
+                    <textarea name="outputSpec" cols="30" rows="5" className="form-control" placeholder="Output description"></textarea>
                 </div>
-                <div class="form-group">
-                    <p class="lead">Upload validator script</p>
-                    <input type="file" class="form-control-file" />
+                <div className="form-group">
+                    <p className="lead">Upload validator script</p>
+                    <input type="file" className="form-control-file" />
                 </div>
-                <div class="test-case">
-                    <p class="lead">Test case #1</p>
-                    <input type="file" class="form-control-file" />
-                    <div class="form-group case-action">
-                        <button class="btn btn-danger">Delete Test Case #1</button>
+                <div className="test-case">
+                    <p className="lead">Test case #1</p>
+                    <input type="file" className="form-control-file" />
+                    <div className="form-group case-action">
+                        <button className="btn btn-danger">Delete Test Case #1</button>
                     </div>
                 </div>
-                <div class="test-case">
-                    <p class="lead">Test case #2</p>
-                    <input type="file" class="form-control-file" />
-                    <div class="form-group case-action">
-                        <button class="btn">Add new test case</button>
-                        <button class="btn btn-danger">Delete Test Case #2</button>
+                <div className="test-case">
+                    <p className="lead">Test case #2</p>
+                    <input type="file" className="form-control-file" />
+                    <div className="form-group case-action">
+                        <button className="btn">Add new test case</button>
+                        <button className="btn btn-danger">Delete Test Case #2</button>
                     </div>
                 </div>
-                <div class="hint">
-                    <p class="lead">Hint #1</p>
-                    <textarea name="inputSpec" cols="30" rows="3" class="form-control" placeholder="Hint #1"></textarea>
-                    <div class="form-group case-action">
-                        <button class="btn btn-danger">Delete Hint #1</button>
+                <div className="hint">
+                    <p className="lead">Hint #1</p>
+                    <textarea name="inputSpec" cols="30" rows="3" className="form-control" placeholder="Hint #1"></textarea>
+                    <div className="form-group case-action">
+                        <button className="btn btn-danger">Delete Hint #1</button>
                     </div>
                 </div>
-                <div class="hint">
-                    <p class="lead">Hint #2</p>
-                    <textarea name="inputSpec" cols="30" rows="3" class="form-control" placeholder="Hint #1"></textarea>
-                    <div class="form-group case-action">
-                        <button class="btn">Add new hint</button>
-                        <button class="btn btn-danger">Delete Hint #2</button>
+                <div className="hint">
+                    <p className="lead">Hint #2</p>
+                    <textarea name="inputSpec" cols="30" rows="3" className="form-control" placeholder="Hint #1"></textarea>
+                    <div className="form-group case-action">
+                        <button className="btn">Add new hint</button>
+                        <button className="btn btn-danger">Delete Hint #2</button>
                     </div>
                 </div>
-                <input type="submit" class="btn btn-primary my-4" />
+                <input type="submit" className="btn btn-primary my-4" />
             </form>
         </Fragment>
     )
