2ebdba2bda3a276464c74eaede89b279953b4d9c
Mike Nguyen <mikenguyen7268@gmail.com>
2021-01-05 20:51:07 -0600

    Modified Auth Redux Actions to fix inconsistency between current tab and current route

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -15,6 +15,7 @@ export const updateUser = (token) => async dispatch => {
     } 
     catch (error) {
         dispatch({ type: LOGIN_FAILED });
+        dispatch(resetTab());
         console.error(error);
     }
 }
@@ -28,6 +29,7 @@ export const loginUser = (data) => async dispatch => {
     } 
     catch (error) {
         dispatch({ type: LOGIN_FAILED });
+        dispatch(resetTab());
         console.error(error);
     }
 }
